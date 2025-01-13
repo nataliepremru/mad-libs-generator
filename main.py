c711f6dfb8040868f859da0d4c2f1c033c07bb1e
pkdc <pkdchoy@gmail.com>
2025-01-13 14:21:13 +0000

    created basic structure

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+def mad_libs_generator():
+    ## Creating the mad lib generator
+    try:
+
+        story = ""
+
+
+        # Asking the user for input
+
+    except ValueError:
+        print("Please enter a valid input")
+
+
+mad_libs_generator()
